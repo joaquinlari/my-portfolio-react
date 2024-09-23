b557755f5cc07e00ed9e4ef7c856b6c7a9f641e5
joaquinlari <larijoaquin@gmail.com>
2024-09-22 23:30:43 -0300

    styling header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,19 @@
+import './Header.css'
+
 export const Header = () => {
   return (
-    <div>
+    <div className="header">
       <h1 className="text-3xl">
         Header
       </h1>
-      <ul>
-        <li>
+      <ul className="list-item">
+        <li className="item">
           <a href="/">Home</a>
         </li>
-        <li>
+        <li className="item">
           <a href="/">Proyectos</a>
         </li>
-        <li>
+        <li className="item">
           <a href="/">Contacto</a>
         </li>
       </ul>
